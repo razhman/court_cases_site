ad9a863593d8afaf040a4f979fe1b031d17414e2
Bors1k <8916misha@gmail.com>
2022-06-27 16:33:58 +0300

    Update settings.py

--- backend/court_cases_backend/court_cases_backend/settings.py
+++ backend/court_cases_backend/court_cases_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import password
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--*-pv()a00ype(do%_o#)67+69b7%r8z702db(4dk7b5f%08a+'
+SECRET_KEY = password.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
